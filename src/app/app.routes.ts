97644ac3b08abb878663110b53232215a4a05ad2
Anil Chandra Giri <123819979+Anil-Chandra-Giri@users.noreply.github.com>
2025-03-27 18:08:08 +0545

    M2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { LogInComponent } from './Components/log-in/log-in.component';
 import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { EmployerDashboardComponent } from './Components/employer-dashboard/employer-dashboard.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent,pathMatch:'full'},
     {path:'log-in',component:LogInComponent,pathMatch:'full'},
-    {path:'sign-up',component:SignUpComponent,pathMatch:'full'}
+    {path:'sign-up',component:SignUpComponent,pathMatch:'full'},
+    {path:'employer-dashboard',component:EmployerDashboardComponent,pathMatch:'full'}
 ];
